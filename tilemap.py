855cd6a7e83700a6af25f315e05d3c4eeeb41d95
PrismaticPolygon <domwhite97@gmail.com>
2019-12-14 18:01:10 +0000

    Added player hitbox

--- tilemap.py
+++ tilemap.py
@@ -1,6 +1,11 @@
 import pygame as pg
 from settings import TILESIZE, WIDTH, HEIGHT
 
+
+def collide_hit_rect(one, two):
+
+    return one.hit_rect.colliderect(two.rect)
+
 class Map:
 
     def __init__(self, filename):
@@ -34,8 +39,8 @@ class Camera:
 
     def update(self, target):
 
-        x = -target.rect.x + int(WIDTH / 2)
-        y = -target.rect.y + int(HEIGHT / 2)
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
 
         # limit scrolling to map size
 
